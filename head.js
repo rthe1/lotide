7fd44233992d18e0cfba973ede8fb94062db1577
Reshawn <reshawnhamilton97@gmail.com>
2021-11-24 05:31:21 +0000

    template literals replaced concatenated string in assertEqual New function added called head()

--- head.js
+++ head.js
@@ -6,8 +6,9 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+function head(arr){
+return arr[0]
+}
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
