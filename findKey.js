be99cddbb5e29a68070ed6fe3c406d09daf7046c
Reshawn <reshawnhamilton97@gmail.com>
2021-11-27 12:38:07 +0000

    added findkeys.js

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`⚠️⚠️⚠️ Assertion Failed  ${actual}  !==  ${expected}`)
+  } else {
+    console.log(`✅✅✅ Assertion Passed  ${actual}  ===  ${expected}`)
+  }
+};
+
+function findKey(object, func) {
+  let keys = Object.keys(object)
+  let result;
+  for (const key of keys) {
+    if (func(object[key])) {
+      result = key
+      return result
+    }
+  }
+
+}
+
+//Test
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
