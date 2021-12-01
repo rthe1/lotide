665f99f9bc3f909e2e42612f09f8a625decbb111
Reshawn <reshawnhamilton97@gmail.com>
2021-12-01 04:07:39 +0000

    middle.js refactored &  test added

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle')
+
+
+
+
+
+
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
